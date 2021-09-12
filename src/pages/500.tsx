d2811422cfff09206f2a1862a108ea257b3b1d39
Tyler Hancock <darklime@live.ca>
2021-09-11 18:40:50 -0600

    Add title to 404 and 500 pages.

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -1,15 +1,11 @@
-import React, { ReactElement} from "react";
+import React, { ReactElement } from "react";
 import Layout from "../components/Layout";
 
 export default function Spooky500(): ReactElement {
 
     return (
-        <Layout>
-            <div className="w-4/6 bg-gray-800 p-8 mt-12 text-gray-200 text-xl flex flex-col gap-y-5 text-center">
-
-                <h1 className="text-5xl text-gray-100">500 - Server Error</h1>
-                <p>A server side error occurred!</p>
-            </div>
+        <Layout title='500 - Internal Server Error'>
+            <p>A server side error occurred!</p>
         </Layout>
     );
 }
