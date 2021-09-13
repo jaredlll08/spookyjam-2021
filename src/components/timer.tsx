3576b9570e0fb7b9a6513a07dbd350399062175d
Tyler Hancock <darklime@live.ca>
2021-09-13 14:51:04 -0600

    Add a timetable for important event dates.

---
+++ src/components/timer.tsx
@@ -0,0 +1,29 @@
+import React, { ReactElement } from "react";
+
+export default function Timer({ days, hours, minutes, seconds }: { days: number, hours: number, minutes: number, seconds: number }): ReactElement {
+
+    return (
+        <div className="inline-flex justify-center text-center px-4 gap-x-4 sm:gap-x-8" id='timer'>
+
+            <div className="my-auto">
+                <p className="text-2xl lg:text-5xl">{days}</p>
+                <p className='font-bold'>Days</p>
+            </div>
+
+            <div className="my-auto">
+                <p className="text-2xl lg:text-5xl">{hours}</p>
+                <p className='font-bold'>Hours</p>
+            </div>
+
+            <div className="my-auto">
+                <p className="text-2xl lg:text-5xl">{minutes}</p>
+                <p className='font-bold'>Minutes</p>
+            </div>
+
+            <div className="my-auto">
+                <p className="text-2xl lg:text-5xl">{seconds}</p>
+                <p className='font-bold'>Seconds</p>
+            </div>
+        </div>
+    );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,54 +1,19 @@
 import React, { ReactElement, ReactNode } from "react";
 import Layout from "../components/Layout";
-import Countdown, { CountdownRenderProps } from 'react-countdown';
+import TimeTableLoader from '../components/timetable_loader'
 
 export default function Home(): ReactElement {
 
-    return <Layout title = "SpookyJam 2021">
+    return <Layout title="SpookyJam 2021">
+
         <div className="">
             <p>
                 Welcome to SpookyJam! This is MMD's annual fall celebration for the modded Minecraft community. During October we host a mod jam where members of the community are encouraged to create new mods that fit the seasonal vibe. Participants are encouraged to collaborate, explore new skills, and challenge themselves. During SpookyJam we also host a public Minecraft server where anyone can join and check out the valid mod jam submissions.
             </p>
-
-<br></br>
-            
-            <Countdown date={Date.UTC(2021, 9, 1, 0, 0, 1)} renderer={renderCountdown}></Countdown>
+        </div>
+        <br />
+        <div id='countdown'>
+            <TimeTableLoader/>
         </div>
     </Layout>
-}
-
-function renderCountdown(props: CountdownRenderProps): ReactNode {
-
-    return (
-
-        <div className="inline-flex w-auto text-center">
-
-            <div className="w-2/12">
-                <p className = "lg:text-5xl sm:text-xs">{props.days}</p>
-                <p>Days</p>
-            </div>
-
-            <p className="md:text-6xl">|</p>
-
-
-            <div className="w-2/12">
-                <p className = "lg:text-5xl sm:text-xs">{props.hours}</p>
-                <p>Hours</p>
-            </div>
-
-            <p className="md:text-6xl">|</p>
-
-            <div className="w-2/12">
-                <p className = "lg:text-5xl sm:text-xs">{props.minutes}</p>
-                <p>Minutes</p>
-            </div>
-
-            <p className="md:text-6xl">|</p>
-
-            <div className="w-2/12">
-                <p className = "lg:text-5xl sm:text-xs">{props.seconds}</p>
-                <p>Seconds</p>
-            </div>
-        </div>
-    );
 }
