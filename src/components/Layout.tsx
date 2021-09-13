9c8bdd86e2c18defd8fbfba0e135fba08ee6e731
Tyler Hancock <darklime@live.ca>
2021-09-13 14:50:38 -0600

    Fix some aspects of layout on small screens.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -16,11 +16,11 @@ export default function Layout({ children, title }: React.PropsWithChildren<Layo
 
         <main className={`flex-grow flex justify-center bg-[#1F1F1F] spooky`}>
 
-            <div className='w-[96%] lg:w-4/6 mt-2 lg:mt-12'>
+            <div className='w-[96%] sm:w-4/6 mt-2 sm:mt-12'>
 
-                <div className='lg:p-8 p-4 bg-gray-800 text-gray-200 text-lg lg:text-xl'>
+                <div className='sm:p-8 p-4 bg-gray-800 text-gray-200 text-lg sm:text-xl'>
 
-                    <h1 className="text-3xl lg:text-5xl mb-3 lg:mb-5 text-gray-100">{title}</h1>
+                    <h1 className="text-3xl lg:text-5xl mb-3 sm:mb-5 text-gray-100">{title}</h1>
                     {children}
                 </div>
             </div>
