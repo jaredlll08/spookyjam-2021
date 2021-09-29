ee2484fb95a57fc04aa8f3b4f21fdf3c30b11e15
Jared <jaredlll08@gmail.com>
2021-09-29 20:33:07 +0200

    add analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,9 @@ class MyDocument extends Document {
         return (
             <Html lang = {"en"}>
 
-                <Head/>
+                <Head>
+                    <script async defer data-website-id="d3c22505-2ae2-413d-8c6d-97e74669575f" src="https://analytics.blamejared.com/umami.js"/>
+                </Head>
                 <body>
                 <Main/> <NextScript/>
                 </body>
