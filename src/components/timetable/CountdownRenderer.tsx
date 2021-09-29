e827d61065c2f522cb7b8e295299adc99c748c9f
Jared <jaredlll08@gmail.com>
2021-09-29 17:25:57 +0200

    fixed countdown always being plural

--- src/components/timetable/CountdownRenderer.tsx
+++ src/components/timetable/CountdownRenderer.tsx
@@ -13,5 +13,12 @@ function renderTimer(props: CountdownRenderProps): ReactNode {
         return <p>This date has passed.</p>;
     }
 
-    return <p>{props.days} days, {props.hours} hours, {props.minutes} minutes, {props.seconds} seconds</p>
+    return <p>{props.days} {pluralize("day", props.days)}, {props.hours} {pluralize("hour", props.hours)}, {props.minutes} {pluralize("minute", props.minutes)}, {props.seconds} {pluralize("second", props.seconds)}</p>
+}
+
+function pluralize(text: string, time: number): string {
+    if(time === 1) {
+        text += "s";
+    }
+    return text;
 }
